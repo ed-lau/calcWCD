fee0d90b85f8fe985401d8f402968f9a8ea6629d
Edward Lau <edward.lau@me.com>
2023-07-14 09:20:03 -0600

    changed to using EuropePMC API

--- R/main.R
+++ R/main.R
@@ -5,6 +5,7 @@
 #' @param year integer() An integer specifying the cutoff year. DEFAULT is now
 #' @param weighted logical() Whether to calculate weighted co-publication factor (T) or non-weighted co-publication factor (F). DEFAULT is T
 #' @keywords semantic similarity
+#' @returns weighted correlation table
 #' @export
 #' @examples
 #' wcd(pmids = test_pmids, annot = test_annotation)
@@ -88,26 +89,32 @@ wcd<- function(pmids,
 
 
 #' @title pmid
-#' @description Retrieves a list of PMIDs from the NCBI EUtils API based on a search term
+#' @description Retrieves a list of PMIDs from the EuropePMC REST API based on a search term.
+#' Note that as of 2023, NCBI EUtils E Search only returns the first 10,000 entries.
+#' Hence we now use the EuropePMC API which returns 1000 results per page but allows multiple page retrieval
+#' Because of the slower speed we now add a max_retrieval option
 #' @param q character() A character string specifying search topic
+#' @param max_retrieval integer() An integer specifying the maximum number of retrieved PMIDs
 #' @keywords PubMed query
+#' @returns A tibble containing one column named pmid, with the list of retrieved PMIDs.
 #' @export
 #' @examples
-#' pmid("fibrosis NOT cystic")
+#' pmid("fibrosis NOT cystic", max_retrieval=1000)
 
 
-pmid <- function(q, apikey){
+pmid <- function(q,
+                 max_retrieval = Inf){
 
   require(dplyr)
   require(httr)
-  require(foreach)
-  require(doParallel)
-  require(iterators)
-  require(snow)
-  require(xml2)
+  # require(foreach)
+  # require(doParallel)
+  # require(iterators)
+  # require(snow)
+  # require(xml2)
 
-  cl <- makeCluster(4, type='SOCK', outfile="")
-  registerDoParallel(cl)
+  # cl <- makeCluster(4, type='SOCK', outfile="")
+  # registerDoParallel(cl)
 
 
   # 2018-04-24 Fix hyphen
@@ -116,45 +123,99 @@ pmid <- function(q, apikey){
   # 2017-08-25 sanitize all other pucntuations
   q <- gsub("[[:punct:]]", "", q)
 
+  # 2023-07-13 for the European PMC Rest API, replace space with %20 in the URL
+  q <- gsub(" ","%20", q)
+
   # 2017-09-06 change space to + sign after sanitizing other punctuations, not before, otherwise queries with more than one words won't work
-  q <- gsub(" ","+", q)
+  # q <- gsub(" ","+", q)
 
-  pub_count_address <- paste0("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=", q, "&rettype=count")
-
-  # NewL: 2018-04-20: use httr and xml2 to get pub count
-  pc <- httr::RETRY("GET", pub_count_address)
-  total_pub_count <- xml2::xml_contents(xml2::xml_children(httr::content(pc))) %>% as.character() %>% as.integer()
-
-  # Quit if no PMIDs
-  #if(total_pub_count == 0){return(data.frame(pmid = character()))}
-  if(total_pub_count == 0){stop("The query resulted in no retrievable publication.")}
-
-  ret_interval <- min(99999, total_pub_count)
-  ret_times <- total_pub_count %/% ret_interval
+  # pub_count_address <- paste0("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=", q, "&rettype=count")
+  #
+  # # NewL: 2018-04-20: use httr and xml2 to get pub count
+  # pc <- httr::RETRY("GET", pub_count_address)
+  # total_pub_count <- xml2::xml_contents(xml2::xml_children(httr::content(pc))) %>% as.character() %>% as.integer()
+  #
+  # print(total_pub_count)
+  #
+  # # Quit if no PMIDs
+  # #if(total_pub_count == 0){return(data.frame(pmid = character()))}
+  # if(total_pub_count == 0){stop("The query resulted in no retrievable publication.")}
+  #
+  # ret_interval <- min(9999, total_pub_count)
+  # ret_times <- total_pub_count %/% ret_interval
 
 
   # Alternative 2018-04-20: now using httr and xml2 to retrieve and read the results, with parallelization and automatic retry.
+  # row_vec = seq(from=0, to=ret_times, length=ret_times+1)
+  # all_pmids <- foreach (this_row=row_vec, .combine='c', .packages=c('httr')) %dopar%{
+  #
+  #   pmid_address <- paste0("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=",q,"&retmax=",
+  #                          ret_interval, "&retstart=", this_row*ret_interval, '&apikey=', apikey)
+  #   print(pmid_address)
+  #   # Retrieve the address content, with retry
+  #   ret = httr::RETRY("GET", pmid_address)
+  #   # Read the retrieved content as XML, then read the contents of the XML children nodes
+  #   ret_content = xml2::xml_contents(xml2::xml_children(httr::content(ret)))
+  #   # Read the contents of the Id fields again to get the PMID names
+  #   as.character(xml2::xml_contents(ret_content))
+  # }
+  # stopCluster(cl)
 
-  row_vec = seq(from=0, to=ret_times, length=ret_times+1)
-  all_pmids <- foreach (this_row=row_vec, .combine='c', .packages=c('httr')) %dopar%{
+  #
 
-    pmid_address <- paste0("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=",q,"&retmax=",
-                           ret_interval, "&retstart=", this_row*ret_interval, '&apikey=', apikey)
-    print(pmid_address)
-    # Retrieve the address content, with retry
-    ret = httr::RETRY("GET", pmid_address)
-    # Read the retrieved content as XML, then read the contents of the XML children nodes
-    ret_content = xml2::xml_contents(xml2::xml_children(httr::content(ret)))
-    # Read the contents of the Id fields again to get the PMID names
-    as.character(xml2::xml_contents(ret_content))
+
+
+  read_result <- function(ret, lis){
+    # """
+    # Get PMIDs from the returned page
+    # ret: retrieved content
+    # lis: total list
+    # """
+    if(length(ret_content$resultList$result) > 0){
+      for(i in 1:length(ret$resultList$result)){
+          lis <- c(lis, ret$resultList$result[[i]]$pmid)
+
+      }
+    }
+    return (lis)
   }
 
-  # Convert to data.frame for backward compatibility
-  all_pmids <- tibble(pmid=all_pmids)
 
-  stopCluster(cl)
+  total_list <- c()
+  # Initial cursor
+  cur <- "*"
+  # Initial URL
+  next_url <- paste0("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=", q, "&resultType=idlist&synonym=FALSE&cursorMark=", cur, "&pageSize=1000&format=json&sort_cited:y")
+
+
+  # Loop through the pages while there is a "next_url" and we haven't downloaded enough PMIDs to reach the max_retrieval yet.
+  while(!is.null(next_url) & length(total_list) < max_retrieval){
+    ret = httr::RETRY("GET", gsub(" ", "%20", next_url))
+    ret_content = httr::content(ret, type  = "application/json")
+
+    pc <- ret_content$hitCount
+    if(pc == 0){ stop("The query resulted in no retrievable publication.")}
+
+    total_list <- read_result(ret_content, total_list)
+
+    print(paste0(length(unique(total_list)), " of ", pc))
+
+    next_url <- ret_content$nextPageUrl
+
+  }
+
+
+
+  # Convert to data.frame for backward compatibility
+  all_pmids <- tibble(pmid=total_list)
+
+
 
   #all_pmids <- mutate(all_pmids, pmid = as.integer(pmid))
   return(all_pmids)
 }
+
+# Time comparison
+# system.time(pmid("heart failure", max_retrieval=10000))
+# system.time(europepmc::epmc_search("heart failure", limit = 10000))
 
